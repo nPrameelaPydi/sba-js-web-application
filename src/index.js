11020ccca09801968665073b466f8e1baaf67607
Prameela Pydi <nprameela.pydi@gmail.com>
2024-10-15 08:16:46 -0700

    tested dummyjson.com

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
+//fetch('https://dummyjson.com/posts')
+//    .then(response => response.json())
+//    .then(data => console.log(data))
+//    .catch(error => console.error('Error:', error));
+
+//to fetch posts from DummyJSON
+const fetchPosts = async () => {
+    const response = await fetch('https://dummyjson.com/posts');
+    const data = await response.json();
+    return data.posts; // Access the posts array
+};
+
+fetchPosts().then(posts => {
+    console.log(posts); // Log the posts to see the output
+});
+
+
+
+
+
+
